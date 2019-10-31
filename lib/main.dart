8d5392eeb8cf984db76d6c0722a34992c649c4db
Kevin <mrhallonline@gmail.com>
2019-10-31 18:17:54 -0500

    add sound functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,85 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound (sound){
+    final player = AudioCache();
+    player.play(sound);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               FlatButton(
-                padding: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),              
+                color: Colors.red,
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
+                  playSound('note1.wav');
                 },
-                child: Container(
-                  height: 50.0,
-                  width: double.infinity,
-                  color: Colors.red,
-                ),
               ),
               FlatButton(
                 padding: EdgeInsets.all(0),
+                color: Colors.orange,
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
+                  playSound('note2.wav');
                 },
-                child: Container(
-                  height: 50.0,
-                  width: double.infinity,
-                  color: Colors.orange,
-                ),
               ),
               FlatButton(
+                  color: Colors.yellow,
                 padding: EdgeInsets.all(0),
                 onPressed: (){
-                  final player = AudioCache();
-                  player.play('note3.wav');
+                  playSound('note3.wav');
                 },
                 child: Container(
                   height: 50.0,
                   width: double.infinity,
-                  color: Colors.yellow,
+                ),
+              ),
+              FlatButton(
+                  color: Colors.green,
+                padding: EdgeInsets.all(0),
+                onPressed: (){
+                  playSound('note4.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                ) ,
+              ),
+              FlatButton(
+                  color: Colors.blue,
+                padding: EdgeInsets.all(0),
+                onPressed: (){
+                  playSound('note5.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                ),
+              ),
+              FlatButton(
+                  color: Colors.indigo,
+                padding: EdgeInsets.all(0),
+                onPressed: (){
+                  playSound('note6.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                ),
+              ),
+              FlatButton(
+                  color: Colors.purple,
+                padding: EdgeInsets.all(0),
+                onPressed: (){
+                  playSound('note7.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
                 ),
               ),
             ],
