78660b06903951e19e583f40475c84b358078994
Kevin <mrhallonline@gmail.com>
2019-10-31 17:10:03 -0500

    add audio player and keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,46 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                padding: EdgeInsets.all(0),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+              ),
+              FlatButton(
+                padding: EdgeInsets.all(0),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  color: Colors.orange,
+                ),
+              ),
+              FlatButton(
+                padding: EdgeInsets.all(0),
+                onPressed: (){
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  color: Colors.yellow,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
